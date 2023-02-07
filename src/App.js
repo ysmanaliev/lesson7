499cf3877bd7ad7eb3eef200c2d7bf4ee2b56d7f
ysmanaliev <marlen002345@gmail.com>
2023-02-07 23:46:40 +0600

    Сделал 7 задание

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//  1- Пример 
+
+
+
+
+// import React from "react";
+
+// class App extends React.Component {
+
+	
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.intervalFlag = null;
+
+// 		this.state = {
+// 			title: "Текущее время",
+// 			date: new Date(),
+// 			counter: 0,
+// 		};
+// 	}
+
+// 	componentDidMount() {
+// 		this.intervalFlag = setInterval(() => {
+// 			this.setState((state, props) => ({
+// 				date: new Date(),
+// 				counter: state.counter + 1,
+// 			}));
+// 		}, 1000);
+// 	}
+
+// 	componentDidUpdate() {}
+
+// 	componentWillUnmount() {
+// 		clearInterval(this.intervalFlag);
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<p>{this.state.title}</p>
+// 				<p>{this.state.counter}</p>
+// 				<p>{this.state.date.toLocaleTimeString()}</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+// export default App;
+
+
+// 2 Пример 
+
+import React, { Component } from 'react'
+
+export default class DidMount extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: 'Этот текст будет удален через 5 секунд '
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({name: "Текст был удален и сработал метот componentDidMount "})
+    }, 5000)
+
+  }
+  render() {
+    return (
+      <div>
+       <p>{this.state.name}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
